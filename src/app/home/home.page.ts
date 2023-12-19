d963f11adba0e82dbdf769818f87abea49a174de
EngiBeerHub <engibeer0422@gmail.com>
2023-12-20 00:56:51 +0900

    Implemented Ionic Storage and SQLite

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {Component, OnInit} from '@angular/core';
+import {IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonText} from '@ionic/angular/standalone';
+import {Storage} from "@ionic/storage-angular";
+import * as CordovaSQLiteDriver from 'localforage-cordovasqlitedriver';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonText],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  count = 0;
+
+  constructor(private storage: Storage) {
+  }
+
+  async ngOnInit() {
+    await this.storage.defineDriver(CordovaSQLiteDriver);
+    await this.storage.create();
+    console.log(`Storage driver: ${this.storage.driver}`);
+  }
+
+  async onClickCountUp() {
+    await this.storage.set('count', ++this.count);
+    const value = await this.storage.get('count');
+    console.log(`Stored value: ${value}`);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import { enableProdMode } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {enableProdMode, importProvidersFrom} from '@angular/core';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {RouteReuseStrategy, provideRouter} from '@angular/router';
+import {IonicRouteStrategy, provideIonicAngular} from '@ionic/angular/standalone';
 
-import { routes } from './app/app.routes';
-import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
+import {routes} from './app/app.routes';
+import {AppComponent} from './app/app.component';
+import {environment} from './environments/environment';
+import {IonicStorageModule} from "@ionic/storage-angular";
+import * as CordovaSQLiteDriver from 'localforage-cordovasqlitedriver';
+import {Drivers} from "@ionic/storage";
 
 if (environment.production) {
   enableProdMode();
@@ -13,8 +16,15 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
     provideIonicAngular(),
     provideRouter(routes),
+    importProvidersFrom(IonicStorageModule.forRoot({
+      name: '__mydb',
+      driverOrder: [
+        CordovaSQLiteDriver._driver,
+        Drivers.IndexedDB,
+        Drivers.LocalStorage]
+    }))
   ],
 });
